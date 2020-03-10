dbb983a980b7d4499d028385fcb27e439e99dae5
Dev Govindji <friendsofdev94@gmail.com>
2020-03-10 16:31:38 -0500

    second commit

---
+++ server.js
@@ -0,0 +1,23 @@
+// Require modeules
+const express = require('express');
+
+// Require Express app
+const app = express();
+
+
+// Configure app (app.set)
+
+// Mount middleware (app.use)
+
+// Mount root route
+app.get('/', function (req, res) {
+    res.send('<h1>Hello World!</h1>');
+});
+
+// Mount other routes
+
+
+// Tell the app to listen on port 3000
+app.listen(3000, function () {
+    console.log('Listening on port 3000');
+});
